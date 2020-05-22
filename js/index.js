5837ca499cc2c13b3d966021abb2772c0b42c616
leegeunhyeok <dev.ghlee@gmail.com>
2020-05-22 13:38:15 +0900

    INIT: Webpack development env

---
+++ js/index.js
@@ -0,0 +1,5 @@
+import '@/css/index.scss'
+
+window.onload = () => {
+  console.log('Loaded')
+}
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './js/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+    publicPath: process.env.NODE_ENV === 'production' ? '/card/dist/' : '/'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_module/,
+        use:{
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(sc|c)ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+       {
+        test: /\.(png|jpe?g|gif|svg|ico)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              useRelativePath: true
+            }
+          }
+        ]
+       }
+    ]
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname)
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  devServer: {
+    port: 9090,
+    inline: true
+  }
+}
