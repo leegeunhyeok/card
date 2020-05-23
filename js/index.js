e6c7246e6abd35a5fb62dc230dbb5ce53ef14f71
leegeunhyeok <dev.ghlee@gmail.com>
2020-05-23 14:42:21 +0900

    UPDATE: Resize handler, Default card layout
    Add window.onresize handler, basic layout (scss)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,35 @@
 import '@/css/index.scss'
 
 window.onload = () => {
-  console.log('Loaded')
+  const card = document.getElementById('card')
+  const logo = document.getElementById('logo')
+  const info = document.getElementById('info')
+  const resizeEventDelay = 10
+  const cardSize = {
+    width: 0,
+    height: 0
+  }
+  let t // timeout
+
+  const resizeHandler = () => {
+    // Card ratio 1.58:1
+    cardSize.width = card.getBoundingClientRect().width
+    cardSize.height = cardSize.width * 1.58
+
+    // Card Size update
+    card.style.height = cardSize.height + 'px'
+    card.style.padding = cardSize.height * 0.04 + 'px'
+
+    // Card layout update
+    logo.style.top = cardSize.height * 0.1 + 'px'
+    info.style.bottom = cardSize.height * 0.03 + 'px'
+  }
+  resizeHandler()
+
+  window.onresize = () => {
+    clearTimeout(t)
+    t = setTimeout(resizeHandler, resizeEventDelay)
+  }
 }
 
 if ('serviceWorker' in navigator) {
