20749ea5b22518d2402393e86ba4c82bef2360bd
leegeunhyeok <dev.ghlee@gmail.com>
2020-05-24 19:05:01 +0900

    UPDATE: Add my name

--- js/index.js
+++ js/index.js
@@ -5,6 +5,11 @@ import '@/css/index.scss'
 const app = new function () {
   const els = {}
 
+  const config = {
+    cardBaseSize: 600,
+    ratio: 1.58
+  }
+
   const resizeConfig = {
     RESIZE_EVENT_DELAY: 10,
     timeout: null
@@ -18,7 +23,7 @@ const app = new function () {
   const init = () => {
     els.app = document.getElementById('app')
     els.card = document.getElementById('card')
-    els.logo = document.getElementById('logo')
+    els.me = document.getElementById('me')
     els.info = document.getElementById('info')
     els.menu = document.getElementById('menu')
 
@@ -35,35 +40,46 @@ const app = new function () {
 
   const resizeHandler = () => {
     clearTimeout(resizeConfig.timeout)
-    resizeConfig.timeout = setTimeout(updateCard, resizeConfig.RESIZE_EVENT_DELAY)
+    resizeConfig.timeout = setTimeout(
+      updateCard,
+      resizeConfig.RESIZE_EVENT_DELAY
+    )
   }
 
   const updateCard = () => {
     // Card ratio 1.58:1
     console.log(els)
     cardSize.width = els.card.getBoundingClientRect().width
-    cardSize.height = cardSize.width * 1.58
+    cardSize.height = cardSize.width * config.ratio
 
     // Card Size update
     els.card.style.height = cardSize.height + 'px'
     els.card.style.padding = cardSize.height * 0.04 + 'px'
 
     // Card layout update
-    els.logo.style.top = cardSize.height * 0.1 + 'px'
+    els.me.style.top = cardSize.height * 0.1 + 'px'
     els.info.style.bottom = cardSize.height * 0.03 + 'px'
   }
 
   const save = () => {
+    const scale = config.cardBaseSize / els.card.clientWidth
     d2i.toPng(els.card, {
+      width: config.cardBaseSize,
+      height: parseInt(config.cardBaseSize * config.ratio),
       style: {
         left: '0',
         right: '0',
         bottom: '0',
         top: '0',
-        transform: 'none',
-        webkitTransform: 'none',
+        borderRadius: '0',
+        webkitTransform: `scale(${scale})`,
+        transform: `scale(${scale})`,
+        webkitTransformOrigin: 'top left',
+        transformOrigin: 'top left',
         boxShadow: 'none',
-        marginTop: '0'
+        marginTop: '0',
+        width: card.clientWidth + 'px',
+        height: card.clientHeight + 'px',
       }
     }).then(r => {
       download(r, 'leegeunhyeok.png', 'image/png')
