1e778ffff62f8050e5c33009daa5b2f335b5ef7a
leegeunhyeok <dev.ghlee@gmail.com>
2021-04-12 12:33:52 +0900

    UPDATE: code format, typings

--- js/index.js
+++ js/index.js
@@ -1,170 +1,167 @@
-import d2i from 'dom-to-image'
-import { saveAs } from 'file-saver'
-import logo from '@/images/icon.png'
-import '@/css/index.scss'
+import d2i from 'dom-to-image';
+import { saveAs } from 'file-saver';
+import logo from '@/images/icon.png';
+import '@/css/index.scss';
 
 try {
   addEventListener(
-    'test',
+    '_',
     null,
     Object.defineProperty({}, 'passive', {
-      get: function() {
-        window.supportsPassiveOption = true
-      }
-    })
-  )
+      get: function () {
+        window.supportsPassiveOption = true;
+      },
+    }),
+  );
 } catch (e) {}
 
-const app = new function () {
-  const els = {}
+const app = new (function () {
+  const els = {};
 
   const config = {
     cardBaseSize: 600,
-    ratio: 1.58
-  }
+    ratio: 1.58,
+  };
 
   const resizeConfig = {
     RESIZE_EVENT_DELAY: 10,
-    timeout: null
-  }
-  
+    timeout: null,
+  };
+
   const cardSize = {
     width: 0,
-    height: 0
-  }
+    height: 0,
+  };
 
   // For iOS Safari issue
-  let isFirst = true
+  let isFirst = true;
 
   const init = () => {
-    els.app = document.getElementById('app')
-    els.card = document.getElementById('card')
-    els.me = document.getElementById('me')
-    els.logo = document.getElementById('logo')
-    els.info = document.getElementById('info')
-    els.menu = document.getElementById('menu')
-    els.menuOpen = document.getElementById('menu_open')
-    els.save = document.getElementById('save')
-    els.blog = document.getElementById('blog')
-    els.source = document.getElementById('source')
+    els.app = document.getElementById('app');
+    els.card = document.getElementById('card');
+    els.me = document.getElementById('me');
+    els.logo = document.getElementById('logo');
+    els.info = document.getElementById('info');
+    els.menu = document.getElementById('menu');
+    els.menuOpen = document.getElementById('menu_open');
+    els.save = document.getElementById('save');
+    els.blog = document.getElementById('blog');
+    els.source = document.getElementById('source');
 
     els.card.addEventListener('click', () => {
-      save()
-    })
+      save();
+    });
 
     els.menuOpen.addEventListener('click', () => {
-      toggleMenu()
-    })
+      toggleMenu();
+    });
 
     els.save.addEventListener('click', () => {
-      save()
-    })
+      save();
+    });
 
     els.blog.addEventListener('click', () => {
-      location.href = 'https://geundung.dev'
-    })
+      location.href = 'https://geundung.dev';
+    });
 
     els.source.addEventListener('click', () => {
-      location.href = 'https://github.com/leegeunhyeok/card'
-    })
+      location.href = 'https://github.com/leegeunhyeok/card';
+    });
 
     // For iOS (:active)
-    document.querySelectorAll('.menu__item__panel').forEach(el => {
+    document.querySelectorAll('.menu__item__panel').forEach((el) => {
       el.addEventListener(
         'touchstart',
         () => {},
-        window.supportsPassiveOption ? {
-          passive: true
-        } : false
-      )
-    })
+        window.supportsPassiveOption
+          ? {
+              passive: true,
+            }
+          : false,
+      );
+    });
 
-    els.logo.src = logo
+    els.logo.src = logo;
 
-    updateCard()
-  }
+    updateCard();
+  };
 
   const resizeHandler = () => {
-    clearTimeout(resizeConfig.timeout)
+    clearTimeout(resizeConfig.timeout);
     resizeConfig.timeout = setTimeout(
       updateCard,
-      resizeConfig.RESIZE_EVENT_DELAY
-    )
+      resizeConfig.RESIZE_EVENT_DELAY,
+    );
 
     if (els.app.classList.contains('up')) {
-      els.app.style.transform = `translateY(-${els.menu.clientHeight}px)`
+      els.app.style.transform = `translateY(-${els.menu.clientHeight}px)`;
     }
-  }
+  };
 
   const updateCard = () => {
     // Card ratio 1.58:1
-    cardSize.width = els.card.getBoundingClientRect().width
-    cardSize.height = cardSize.width * config.ratio
+    cardSize.width = els.card.getBoundingClientRect().width;
+    cardSize.height = cardSize.width * config.ratio;
 
     // Card Size update
-    els.card.style.height = cardSize.height + 'px'
-    els.card.style.padding = cardSize.height * 0.04 + 'px'
+    els.card.style.height = cardSize.height + 'px';
+    els.card.style.padding = cardSize.height * 0.04 + 'px';
 
     // Card layout update
-    els.me.style.top = cardSize.height * 0.1 + 'px'
-    els.info.style.bottom = cardSize.height * 0.03 + 'px'
-  }
+    els.me.style.top = cardSize.height * 0.1 + 'px';
+    els.info.style.bottom = cardSize.height * 0.03 + 'px';
+  };
 
   const save = async () => {
-    const w = els.card.clientWidth
-    const scale = config.cardBaseSize / w
+    const w = els.card.clientWidth;
+    const scale = config.cardBaseSize / w;
 
     if (isFirst) {
-      await d2i.toBlob(els.card).then(() => isFirst = false)
+      await d2i.toBlob(els.card).then(() => (isFirst = false));
     }
 
-    d2i.toBlob(els.card, {
-      width: config.cardBaseSize,
-      height: parseInt(config.cardBaseSize * config.ratio),
-      style: {
-        left: '0',
-        right: '0',
-        bottom: '0',
-        top: '0',
-        borderRadius: '0',
-        boxShadow: 'none',
-        webkitTransform: `scale(${scale})`,
-        transform: `scale(${scale})`,
-        webkitTransformOrigin: 'top left',
-        transformOrigin: 'top left'
-      }
-    }).then(b => {
-      saveAs(b, 'leegeunhyeok.png')
-    })
-  }
+    d2i
+      .toBlob(els.card, {
+        width: config.cardBaseSize,
+        height: parseInt(config.cardBaseSize * config.ratio),
+        style: {
+          left: '0',
+          right: '0',
+          bottom: '0',
+          top: '0',
+          borderRadius: '0',
+          boxShadow: 'none',
+          webkitTransform: `scale(${scale})`,
+          transform: `scale(${scale})`,
+          webkitTransformOrigin: 'top left',
+          transformOrigin: 'top left',
+        },
+      })
+      .then((b) => {
+        saveAs(b, 'leegeunhyeok.png');
+      });
+  };
 
   const toggleMenu = () => {
-    els.app.classList.toggle('up')
+    els.app.classList.toggle('up');
     if (els.app.classList.contains('up')) {
-      els.app.style.transform = `translateY(-${els.menu.clientHeight}px)`
+      els.app.style.transform = `translateY(-${els.menu.clientHeight}px)`;
     } else {
-      els.app.style.transform = `none`
+      els.app.style.transform = `none`;
     }
-  }
+  };
 
   return {
     init,
-    resizeHandler
-  }
-}
+    resizeHandler,
+  };
+})();
 
 window.onload = () => {
-  app.init()
-  window.addEventListener('resize', app.resizeHandler)
-}
+  app.init();
+  window.addEventListener('resize', app.resizeHandler);
+};
 
-if (
-  process.env.NODE_ENV !== 'development' &&
-  'serviceWorker' in navigator
-) {
-  navigator.serviceWorker.register('service-worker.js')
-    .then(registration => {
-      console.log(registration)
-    })
-    .catch(console.error)
+if (process.env.NODE_ENV !== 'development' && 'serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js').catch(console.error);
 }
--- service-worker.js
+++ service-worker.js
@@ -1,55 +1,59 @@
-const VERSION = 'v1'
-const CACHE_NAME = 'cache_' + VERSION
+const VERSION = 'v2';
+const CACHE_NAME = 'cache_' + VERSION;
 
-const publicPath = '/card'
+const publicPath = '/card';
 
 const CACHE_LIST = [
   '/',
   '/favicon.ico',
   '/favicon-32x32.png',
   '/manifest.json',
-  '/bundle.js'
-].map(x => publicPath + x)
+  '/bundle.js',
+].map((x) => publicPath + x);
 
-const PATTERN = /images|css/
+const PATTERN = /images|css/;
 
-self.addEventListener('install', event => {
-  console.log('Service Worker - install', VERSION)
+self.addEventListener('install', (event) => {
+  console.log('Service Worker - install', VERSION);
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(CACHE_LIST)
-    })
-  )
-})
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CACHE_LIST);
+    }),
+  );
+});
 
-self.addEventListener('activate', event => {
-  console.log('Service Worker - activate', VERSION)
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker - activate', VERSION);
   event.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(keyList.map(key => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key)
-        }
-      }))
-    })
-  )
-})
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        }),
+      );
+    }),
+  );
+});
 
-self.addEventListener('fetch', event => {
-  console.log('Service Worker - fetch', event.request.url)
-  const pathName = new URL(event.request.url).pathname
-  
+self.addEventListener('fetch', (event) => {
+  console.log('Service Worker - fetch', event.request.url);
+  const pathName = new URL(event.request.url).pathname;
+
   if (CACHE_LIST.includes(pathName) || PATTERN.test(pathName)) {
     event.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-        return cache.match(event.request).then(response => {
-          return response || fetch(event.request)
-            .then(networkResponse => {
-              cache.put(event.request, networkResponse.clone())
-              return networkResponse
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          return (
+            response ||
+            fetch(event.request).then((networkResponse) => {
+              cache.put(event.request, networkResponse.clone());
+              return networkResponse;
             })
-        })
-      })
-    )
+          );
+        });
+      }),
+    );
   }
-})
+});
